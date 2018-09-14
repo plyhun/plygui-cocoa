37626d5c322685a323386d425bddb86a2e2ae826
Serhii Plyhun <snuk188@gmail.com>
2018-09-14 13:44:25 +0200

    Prelude mod

--- src/lib.rs
+++ src/lib.rs
@@ -22,17 +22,28 @@ mod layout_linear;
 mod splitted;
 mod window;
 
-pub use self::application::Application;
-pub use self::button::Button;
-pub use self::frame::Frame;
-pub use self::layout_linear::LinearLayout;
-pub use self::splitted::Splitted;
-pub use self::window::Window;
-
 #[cfg(feature = "markup")]
 pub fn register_members(registry: &mut plygui_api::markup::MarkupRegistry) {
     registry.register_member(plygui_api::markup::MEMBER_TYPE_BUTTON.into(), button::spawn).unwrap();
     registry.register_member(plygui_api::markup::MEMBER_TYPE_LINEAR_LAYOUT.into(), layout_linear::spawn).unwrap();
     registry.register_member(plygui_api::markup::MEMBER_TYPE_FRAME.into(), frame::spawn).unwrap();
     //registry.register_member(plygui_api::markup::MEMBER_TYPE_SPLITTED.into(), splitted::spawn).unwrap();
+}
+
+pub mod prelude {
+	pub use plygui_api::controls::*;
+	pub use plygui_api::ids::*;
+	pub use plygui_api::types::*;
+	pub use plygui_api::callbacks;
+	pub use plygui_api::layout;
+	pub use plygui_api::utils; 
+	
+	pub mod imp {
+		pub use ::application::Application;
+		pub use ::window::Window;
+		pub use ::button::Button;
+		pub use ::layout_linear::LinearLayout;
+		pub use ::frame::Frame;
+		pub use ::splitted::Splitted;
+	}
 }
