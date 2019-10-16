ecb866a45c74233edfa225bf38ee676d3b148606
admin <admin@192.168.192.128>
2019-10-16 23:24:00 +0200

    List OnItemClick blueprint

--- src/list.rs
+++ src/list.rs
@@ -4,8 +4,9 @@ lazy_static! {
     static ref WINDOW_CLASS: common::RefClass = unsafe {
         register_window_class("PlyguiList", BASE_CLASS, |decl| {
             decl.add_method(sel!(setFrameSize:), set_frame_size as extern "C" fn(&mut Object, Sel, NSSize));
+            decl.add_method(sel!(itemClicked:), item_clicked as extern "C" fn(&mut Object, Sel, cocoa_id));
             decl.add_method(sel!(numberOfRowsInTableView:), datasource_len as extern "C" fn(&mut Object, Sel, cocoa_id) -> NSInteger);
-            //decl.add_method(sel!(tableView:objectValueForTableColumn:row:), get_item as extern "C" fn(&mut Object, Sel, cocoa_id, cocoa_id, NSInteger) -> cocoa_id);
+            decl.add_method(sel!(tableView:heightOfRow:), get_item_height as extern "C" fn(&mut Object, Sel, cocoa_id, NSInteger) -> f64);
             decl.add_method(sel!(tableView:viewForTableColumn:row:), spawn_item as extern "C" fn(&mut Object, Sel, cocoa_id, cocoa_id, NSInteger) -> cocoa_id);
         })
     };
@@ -18,7 +19,29 @@ pub type List = Member<Control<Adapter<CocoaList>>>;
 #[repr(C)]
 pub struct CocoaList {
     base: common::CocoaControlBase<List>,
-    children: Vec<Box<dyn controls::Control>>,
+    items: Vec<Box<dyn controls::Control>>,
+}
+
+impl CocoaList {
+    fn add_item_inner(&mut self, base: &mut MemberBase, i: usize) {
+        let (member, control, adapter) = List::adapter_base_parts_mut(base);
+        let (pw, ph) = control.measured;
+        let this: &mut List = unsafe { utils::base_to_impl_mut(member) };
+        
+        let mut item = adapter.adapter.spawn_item_view(i, this);
+        item.on_added_to_container(this, 0, 0, utils::coord_to_size(pw as i32) as u16, utils::coord_to_size(ph as i32) as u16);
+                
+        let (_, yy) = item.size();
+        self.items.insert(i, item);
+        
+        unsafe {
+            let () = msg_send![self.base.control, setRowHeight: yy as f64];
+        }
+    }
+    fn remove_item_inner(&mut self, base: &mut MemberBase, i: usize) {
+        let this: &mut List = unsafe { utils::base_to_impl_mut(base) };
+        self.items.remove(i).on_removed_from_container(this); 
+    }    
 }
 
 impl AdapterViewInner for CocoaList {
@@ -28,7 +51,7 @@ impl AdapterViewInner for CocoaList {
                 Adapter::with_inner(
                     CocoaList {
                         base: common::CocoaControlBase::with_params(*WINDOW_CLASS),
-                        children: Vec::new(),
+                        items: Vec::new(),
                     },
                     adapter,
                 ),
@@ -46,36 +69,31 @@ impl AdapterViewInner for CocoaList {
             let column: cocoa_id = msg_send![column, initWithIdentifier:ident];
             let () = msg_send![control, addTableColumn: column];
             
-            let () = msg_send![control, setDelegate: control];
-            let () = msg_send![control, setDataSource: control];
+            let _ = msg_send![control, setTarget: control];
+            let _ = msg_send![control, setAction: sel!(itemClicked:)];
         }
         ll
     }
     fn on_item_change(&mut self, base: &mut MemberBase, value: types::Change) {
-        let mut y = 0;
-        {
-            for item in self.children.as_slice() {
-                let (_, yy) = item.size();
-                y += yy as i32;
-            }
-        }
         match value {
             types::Change::Added(at) => {
-                //self.add_item_inner(base, at, &mut y);
+                self.add_item_inner(base, at);
             },
             types::Change::Removed(at) => {
-                //self.remove_item_inner(base, at);
+                self.remove_item_inner(base, at);
             },
             types::Change::Edited(_) => {
             },
         }
-        self.base.invalidate();
+        unsafe {
+            let () = msg_send![self.base.control, reloadData];
+        }
     }
 }
 
 impl ContainerInner for CocoaList {
     fn find_control_mut(&mut self, arg: types::FindBy) -> Option<&mut dyn controls::Control> {
-        for child in self.children.as_mut_slice() {
+        for child in self.items.as_mut_slice() {
             match arg {
                 types::FindBy::Id(ref id) => {
                     if child.as_member_mut().id() == *id {
@@ -101,7 +119,7 @@ impl ContainerInner for CocoaList {
         None
     }
     fn find_control(&self, arg: types::FindBy) -> Option<&dyn controls::Control> {
-        for child in self.children.as_slice() {
+        for child in self.items.as_slice() {
             match arg {
                 types::FindBy::Id(ref id) => {
                     if child.as_member().id() == *id {
@@ -131,21 +149,26 @@ impl ContainerInner for CocoaList {
 impl ControlInner for CocoaList {
     fn on_added_to_container(&mut self, member: &mut MemberBase, control: &mut ControlBase, _parent: &dyn controls::Container, x: i32, y: i32, pw: u16, ph: u16) {
         self.measure(member, control, pw, ph);
-        let mut y = y;
+        control.coords = Some((x, y));
+        
+        let (member, _, adapter) = List::adapter_base_parts_mut(member);
 
-        let self2 = unsafe { common::member_from_cocoa_id_mut::<List>(self.base.control).unwrap() };
-        for ref mut child in self.children.as_mut_slice() {
-            unsafe {
-                let () = msg_send![self2.as_inner_mut().as_inner_mut().as_inner_mut().base.control, addSubview: child.native_id() as cocoa_id];
-            }
-            child.on_added_to_container(self2, x, y, control.measured.0, control.measured.1);
-            let (_, yy) = child.size();
-            y += yy as i32;
+        for i in 0..adapter.adapter.len() {
+            self.add_item_inner(member, i);
+        }
+
+        unsafe {                            
+            let () = msg_send![self.base.control, setDelegate: self.base.control];
+            let () = msg_send![self.base.control, setDataSource: self.base.control];
         }
     }
     fn on_removed_from_container(&mut self, _: &mut MemberBase, _: &mut ControlBase, _: &dyn controls::Container) {
+        unsafe {
+            let () = msg_send![self.base.control, setDelegate: nil];
+            let () = msg_send![self.base.control, setDataSource: nil];
+        }
         let ll2: &List = unsafe { common::member_from_cocoa_id(self.base.control).unwrap() };
-        for ref mut child in self.children.as_mut_slice() {
+        for ref mut child in self.items.as_mut_slice() {
             child.on_removed_from_container(ll2);
         }
         unsafe {
@@ -171,7 +194,7 @@ impl ControlInner for CocoaList {
         use plygui_api::markup::MEMBER_TYPE_LINEAR_LAYOUT;
 
         fill_from_markup_base!(self, base, markup, registry, List, [MEMBER_TYPE_LINEAR_LAYOUT]);
-        fill_from_markup_children!(self, base, markup, registry);
+        //fill_from_markup_items!(self, base, markup, registry);
     }
 }
 
@@ -228,7 +251,7 @@ impl Drawable for CocoaList {
                     layout::Size::MatchParent => parent_width,
                     layout::Size::WrapContent => {
                         let mut w = 0;
-                        for child in self.children.as_mut_slice() {
+                        for child in self.items.as_mut_slice() {
                             let (cw, _, _) = child.measure(max(0, parent_width as i32) as u16, max(0, parent_height as i32) as u16);
                             w = max(w, cw);
                         }
@@ -241,7 +264,7 @@ impl Drawable for CocoaList {
                     layout::Size::MatchParent => parent_height,
                     layout::Size::WrapContent => {
                         let mut h = 0;
-                        for child in self.children.as_mut_slice() {
+                        for child in self.items.as_mut_slice() {
                             let ch = if measured {
                                 child.size().1
                             } else {
@@ -265,9 +288,18 @@ impl Drawable for CocoaList {
 extern "C" fn set_frame_size(this: &mut Object, _: Sel, param: NSSize) {
     unsafe {
         let sp = common::member_from_cocoa_id_mut::<List>(this).unwrap();
-        let () = msg_send![super(sp.as_inner_mut().as_inner_mut().as_inner_mut().base.control, Class::get(BASE_CLASS).unwrap()), setFrameSize: param];
+        if let Some(cls) = Class::get(BASE_CLASS) {
+            let () = msg_send![super(sp.as_inner_mut().as_inner_mut().as_inner_mut().base.control, cls), setFrameSize: param];
+            sp.call_on_size(param.width as u16, param.height as u16);
+        }
+    }
+    /*
+    if let Some(sp) = unsafe { common::member_from_cocoa_id_mut::<List>(this) } {
+        unsafe { let () = msg_send![super(sp.as_inner_mut().as_inner_mut().as_inner_mut().base.control, Class::get(BASE_CLASS).unwrap()), setFrameSize: param]; }
         sp.call_on_size(param.width as u16, param.height as u16)
     }
+    
+    */
 }
 /*
 #[allow(dead_code)]
@@ -282,12 +314,34 @@ extern "C" fn datasource_len(this: &mut Object, _: Sel, _: cocoa_id) -> NSIntege
     }
 }
 extern "C" fn spawn_item(this: &mut Object, _: Sel, _: cocoa_id, _: cocoa_id, row: NSInteger) -> cocoa_id {
-    unsafe {
-        let sp = common::member_from_cocoa_id_mut::<List>(this).unwrap();
-        let sp2 = common::member_from_cocoa_id_mut::<List>(this).unwrap();
-        let view = sp.as_inner_mut().as_inner_mut().base_mut().adapter.spawn_item_view(row as usize, sp2);
-        sp.as_inner_mut().as_inner_mut().as_inner_mut().children.insert(row as usize, view);
-        sp.as_inner_mut().as_inner_mut().as_inner_mut().children[row as usize].native_id() as cocoa_id
+    let sp = unsafe { common::member_from_cocoa_id_mut::<List>(this).unwrap() };
+    println!("spawned {}", row);
+    unsafe { sp.as_inner_mut().as_inner_mut().as_inner_mut().items[row as usize].native_id() as cocoa_id }
+}
+extern "C" fn get_item_height(this: &mut Object, _: Sel, _: cocoa_id, row: NSInteger) -> f64 {
+    let sp = unsafe { common::member_from_cocoa_id::<List>(this).unwrap() };
+    /*if let Some(item) = sp.as_inner().as_inner().as_inner().items.get(row as usize) {
+        let (_, h) = item.size();
+        h as f64
+    } else {
+        1.0
+    }*/
+    let (_, h) = sp.as_inner().as_inner().as_inner().items[row as usize].size();
+    println!("height {} = {}", row, h);
+    h as f64
+}
+extern "C" fn item_clicked(this: &mut Object, _: Sel, _: cocoa_id) {
+    let sp = unsafe { common::member_from_cocoa_id_mut::<List>(this).unwrap() };
+    let sp2 = unsafe { common::member_from_cocoa_id_mut::<List>(this).unwrap() };
+    let i: NSInteger = unsafe { msg_send![this, clickedRow] };
+    println!("clicked {}", i);
+    if i < 0 {
+        return;
+    }
+    let item_view = sp.as_inner_mut().as_inner_mut().as_inner_mut().items.get_mut(i as usize).unwrap();
+    if let Some(ref mut callback) = sp2.as_inner_mut().as_inner_mut().base_mut().on_item_click {
+        let sp2 = unsafe { common::member_from_cocoa_id_mut::<List>(this).unwrap() };
+        (callback.as_mut())(sp2, i as usize, item_view.as_mut());
     }
 }
 
